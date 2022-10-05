ab3851285fb7480af4bf998342c03b3d77220c00
Hossein Aflaki <hossein.aflaki70@gmail.com>
2022-10-05 16:39:48 +0330

    first create basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -3,6 +3,14 @@ const BasicForm = () => {
     <form autoComplete="off">
       <label htmlFor="email">Email</label>
       <input id="email" type="email" placeholder="Enter your email" />
+      <label htmlFor="email">Age</label>
+      <input id="email" type="email" placeholder="Enter your age" />
+      <label htmlFor="email">Password</label>
+      <input id="email" type="email" placeholder="Enter your password" />
+      <label htmlFor="email">Confirm Password</label>
+      <input id="email" type="email" placeholder="Confirm Password" />
+      <button>Submit</button>
+      
     </form>
   );
 };
