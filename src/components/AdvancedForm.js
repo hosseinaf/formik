3302c5d3d51db14ef61c35220ef6137776240448
Hossein Aflaki <hossein.aflaki70@gmail.com>
2022-10-05 16:49:10 +0330

    first code for advanced form

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -3,6 +3,16 @@ const AdvancedForm = () => {
     <form autoComplete="off">
       <label htmlFor="username">Username</label>
       <input id="username" type="text" placeholder="Enter your username" />
+
+      <label htmlFor="username">Job Type</label>
+      <select id="country" name="Job Type">
+        <option value="">Please select a job type</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="manager">Product Manager</option>
+            <option value="other">Other</option>
+      </select>
+      <button>Submit</button>
     </form>
   );
 };
